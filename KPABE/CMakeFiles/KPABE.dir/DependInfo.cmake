0728d169158cac21ce34b4b5dc37a6a65d6af108
Tuan <trinhsytuan@gmail.com>
2024-07-26 10:49:23 +0700

    Init project

---
+++ KPABE/CMakeFiles/KPABE.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tuan/CLionProjects/KPABE/Decrypt.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/Decrypt.cpp.o"
+  "/Users/tuan/CLionProjects/KPABE/Encrypt.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/Encrypt.cpp.o"
+  "/Users/tuan/CLionProjects/KPABE/KeyGenerator.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/KeyGenerator.cpp.o"
+  "/Users/tuan/CLionProjects/KPABE/attribute.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/attribute.cpp.o"
+  "/Users/tuan/CLionProjects/KPABE/global_params.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/global_params.cpp.o"
+  "/Users/tuan/CLionProjects/KPABE/main.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/main.cpp.o"
+  "/Users/tuan/CLionProjects/KPABE/utils/utils.cpp" "/Users/tuan/CLionProjects/KPABE/cmake-build-debug/CMakeFiles/KPABE.dir/utils/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/tuan/CLionProjects/KPABE/KPABE"
+  "/opt/homebrew/Cellar/gmp/6.3.0/include"
+  "/opt/homebrew/Cellar/pbc/0.5.14/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
